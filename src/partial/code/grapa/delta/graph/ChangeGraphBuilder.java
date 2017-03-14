e3721dbcb082c0582e1a9b91a7c395a8c8f698df
HaoZhong <zhonghao@sjtu.edu.cn>
2017-03-14 17:46:53 +0800

    leftside bug

--- src/partial/code/grapa/delta/graph/ChangeGraphBuilder.java
+++ src/partial/code/grapa/delta/graph/ChangeGraphBuilder.java
@@ -24,32 +24,34 @@ public class ChangeGraphBuilder extends GraphComparator{
 		DirectedSparseGraph<DeltaNode, DeltaEdge> graph = new DirectedSparseGraph<DeltaNode, DeltaEdge>();
 		//add left nodes
 		for(DeltaNode s:leftGraph.getVertices()){
-			s.side = DeltaNode.LEFT;
+			if(!this.bSwapSide){
+				s.side = DeltaNode.LEFT;
+			}else{
+				s.side = DeltaNode.RIGHT;
+			}
 			graph.addVertex(s);
 		}
 		
 		//add right nodes
 		for(DeltaNode s:rightGraph.getVertices()){
-			s.side = DeltaNode.RIGHT;
+			if(!this.bSwapSide){
+				s.side = DeltaNode.RIGHT;
+			}else{
+				s.side = DeltaNode.LEFT;
+			}
 			graph.addVertex(s);
 		}
 		
-		for(DeltaNode n1:graph.getVertices()){
-			for(DeltaNode n2:graph.getVertices()){
-				if(!n1.equals(n2)&&n1.side==n2.side){
-					DeltaEdge edge;
-					if(n1.side==DeltaNode.LEFT){
-						edge = leftGraph.findEdge(n1, n2);
-					}else{
-						edge = rightGraph.findEdge(n1, n2);
-					}
-					if(edge != null){
-						graph.addEdge(edge, n1, n2);
-					}
-				}			
-			}
+		//add left edges;
+		for(DeltaEdge e:leftGraph.getEdges()){
+			graph.addEdge(e, (DeltaNode)e.from, (DeltaNode)e.to);
+		}
+		//add right edges
+		for(DeltaEdge e:rightGraph.getEdges()){
+			graph.addEdge(e, (DeltaNode)e.from, (DeltaNode)e.to);
 		}
 		
+		//add modification edges
 		for(DeltaNode n1:graph.getVertices()){
 			DeltaNode n2 = vm.get(n1);
 			if(n2!=null){
@@ -57,11 +59,11 @@ public class ChangeGraphBuilder extends GraphComparator{
 					n1.bModified = false;
 					n2.bModified = false;
 				}else{
-					DeltaEdge edge = graph.findEdge(n1, n2);
-					if(edge==null){
-						edge = new DeltaEdge(n1, n2, DeltaEdge.CHANGE);
-						graph.addEdge(edge, n1, n2);
-					}
+					n1.bModified = true;
+					n2.bModified = true;
+					DeltaEdge edge = new DeltaEdge(n1, n2, DeltaEdge.CHANGE);
+					graph.addEdge(edge, n1, n2);
+					
 				}
 			}
 		}
