5166d4a80f865592fc6e89859616eb646d1aeaea
drzhonghao <zhonghao@sjtu.edu.cn>
2017-09-27 09:08:49 +0800

    a

---
+++ src/partial/code/grapa/commit/api/ApiComparator.java
@@ -0,0 +1,17 @@
+package partial.code.grapa.commit.api;
+
+import java.util.ArrayList;
+
+import org.eclipse.jdt.core.dom.ASTNode;
+
+import partial.code.grapa.commit.Comparator;
+
+public class ApiComparator extends Comparator{
+
+	@Override
+	protected void doCompare(ArrayList<ASTNode> leftTrees, ArrayList<ASTNode> rightTrees) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
